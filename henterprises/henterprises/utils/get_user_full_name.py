1feed01878c614fcc815b8d058ecb159a3b0d456
safdar211ali <safdar211@hotmail.com>
2024-04-25 19:04:42 +0500

    upated

--- henterprises/henterprises/utils/get_user_full_name.py
+++ henterprises/henterprises/utils/get_user_full_name.py
@@ -2,6 +2,8 @@
 import frappe
 
 @frappe.whitelist()
-def get_user_full_name(owner):
-    full_name = frappe.db.get_value("User", {"name": owner}, "full_name")
+def get_user_full_name():
+    user = frappe.session.user
+    full_name = frappe.db.get_value("User", {"name": user}, "full_name")
     return full_name
+
